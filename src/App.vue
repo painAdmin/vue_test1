62b960b3d7536f6ac4ea896178065a629e602d1b
sysadmin <1767123925@qq.com>
2018-08-04 14:42:34 +0800

    父组件 向子组件传值

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,25 @@
 <template>
   <div id="app">
-    <h1>测试style</h1>
-    {{msg}}
+
     <!--第三步 在模板中使用-->
     <v-home></v-home>
-    <v-news></v-news>
   </div>
 </template>
 
 <script>
   /*第一步 引入组件*/
   import Home from './components/Home.vue';
-  import News from './components/News.vue';
 
 export default {
   name: 'App',
   data(){
       return{
-         msg:"测试双向数据绑定"
     }
   },
   /*第二部 注册组件*/
   components:{
-    'v-home':Home,
-    'v-news':News
-  },
-//  mounted(){//页面刷新 时候触发
-//      alert('生命周期');
-//  }
+    'v-home':Home
+  }
 
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="header">
-     <h1 v-html="msg"></h1>
+        <h1>{{msg}}  ::: {{title}}::::{{test}}</h1>
+    <button @click="run('子组件方法入参')">方法执行</button>
+    <button @click="getParent()">获取父组件数据+方法+dom</button>
   </div>
 </template>
 <script>
   export default{
       data(){
           return{
-            msg:'我是头部组件'
+            msg:"我是头部组件"
           }
-      }
-  }
+      },methods:{
+          getParent(){
+//              alert(this.title);
+              console.log("执行结束:"+this.home.title);
+          }
 
+    },props:['title','test','run','home']
+  }
 </script>
 <style scoped>
 
